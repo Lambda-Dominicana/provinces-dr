d85d59384323a860527d992d6ff1630403b7a4e9
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-09-21 19:49:17 -0400

    updated documentation of module

--- lib/provinces_dr.ex
+++ lib/provinces_dr.ex
@@ -53,17 +53,57 @@ defmodule ProvincesDr do
 
   @doc """
   Get all provinces.
+
+  ## Examples
+
+      iex> ProvincesDr.provinces()
+      {:ok,
+        [
+          {"1", "Azua"},
+          {"2", "Bahoruco"},
+          {"3", "Barahona"},
+          {"4", "Dajabón"},
+          {"5", "Distrito Nacional"},
+          {"6", "Duarte"},
+          {"7", "Elías Piña"},
+          {"8", "El Seibo"},
+          {"9", "Espaillat"},
+          {"10", "Hato Mayor"},
+          {"11", "Hermanas Mirabal"},
+          {"12", "Independencia"},
+          {"13", "La Altagracia"},
+          {"14", "La Romana"},
+          {"15", "La Vega"},
+          {"16", "María Trinidad Sánchez"},
+          {"17", "Monseñor Nouel"},
+          {"18", "Monte Cristi"},
+          {"19", "Monte Plata"},
+          {"20", "Pedernales"},
+          {"21", "Peravia"},
+          {"22", "Puerto Plata"},
+          {"23", "Samaná"},
+          {"24", "Sánchez Ramírez"},
+          {"25", "San Cristóbal"},
+          {"26", "San José de Ocoa"},
+          {"27", "San Juan"},
+          {"28", "San Pedro de Macorís"},
+          {"29", "Santiago"},
+          {"30", "Santiago Rodríguez"},
+          {"31", "Santo Domingo"},
+          {"32", "Valverde"}
+        ]}
   """
   def provinces()
 
   @doc """
   Get all Municipalities and Municipal Disctrics from a provindece code.
 
+  ## Params
+      code: Code of province
+
   ## Examples
 
       iex> ProvincesDr.province()
-      {:error, "Not foun with this code."}
-      iex> ProvincesDr.province("1000")
       {:error, "Not foun with this code."}
       iex> ProvincesDr.province("2")
       {:ok,
