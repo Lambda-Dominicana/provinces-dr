de6ecf4ad641bdac0ef7228c0487abfa6869e073
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-11-19 14:46:09 -0400

    pblished

--- mix.exs
+++ mix.exs
@@ -27,8 +27,7 @@ defmodule ProvincesDr.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
@@ -42,8 +41,7 @@ defmodule ProvincesDr.MixProject do
   defp package do
     [
       # These are the default files included in the package
-      files: ~w(lib .formatter.exs mix.exs README* readme* LICENSE*
-                license* src),
+      files: ~w(lib .formatter.exs mix.exs README*  LICENSE*  src),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/Lambda-Dominicana/provinces-dr"}
     ]
